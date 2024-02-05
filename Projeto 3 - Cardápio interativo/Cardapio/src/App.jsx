be48fadb5a2954b9e65097bfbce07687bb3d2102
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2024-02-04 21:03:45 -0300

    primeiro componente app

---
+++ Projeto 3 - Cardápio interativo/Cardapio/src/App.jsx
@@ -0,0 +1,9 @@
+import Hashtaurante from "./assets/hashtaurante.webp"
+import "./App.css"
+export function App(){
+  
+  return <>
+          <img src={Hashtaurante} alt="" className="capa" />
+          <h1>Título da minha página em React</h1>
+        </>
+}
